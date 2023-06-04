0361527bfcb1a64397347225c8ee37b9dd6ee0a4
Stephen Adhisaputra <stephenadhi@gmail.com>
2023-06-04 14:27:35 +0200

    Add autonomous control launch file

---
+++ src/vehicle_control/launch/autonomous_control.launch.py
@@ -0,0 +1,52 @@
+
+import os
+import yaml
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
+from launch.substitutions import PythonExpression
+
+def generate_launch_description():
+    control_config_file = os.path.join(
+        get_package_share_directory('vehicle_control'),
+        'config',
+        'control_config.yaml'
+    )
+    with open(control_config_file, 'r') as file:
+        control_config = yaml.safe_load(file)['vehicle_control']['ros_parameters']
+
+    # vesc
+    vesc_driver_node = Node(
+        package='vesc_driver',
+        executable='vesc_driver_node',
+        name='vesc_driver_node',
+        parameters=[control_config]
+    )
+
+    # twist_to_ackermann
+    twist_to_ackermann_node = Node(
+        package='twist_to_ackermann',
+        executable='twist_to_ackermann',
+        name='twist_to_ackermann',
+        output='screen',
+    )
+
+    # ackermann_to_vesc
+    ackermann_to_vesc_node = Node(
+        package='vehicle_control',
+        executable='ackermann_to_vesc.py',
+        name='ackermann_to_vesc',
+        output='screen',
+        parameters=[control_config]
+    )
+
+    return LaunchDescription([
+        vesc_driver_node,
+        twist_to_ackermann_node,
+        ackermann_to_vesc_node
+    ])
+
