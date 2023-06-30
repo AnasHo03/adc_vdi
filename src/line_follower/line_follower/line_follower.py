481b58654c257a694a50980576b7ac0cbfd524db
Stephen Adhisaputra <stephenadhi@gmail.com>
2023-06-30 10:41:22 +0200

    Add shutdown callback and edit config

--- src/line_follower/line_follower/line_follower.py
+++ src/line_follower/line_follower/line_follower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+from platform import node
 import rclpy
+import signal
 from rclpy.node import Node
 from ackermann_msgs.msg import AckermannDrive
 from std_msgs.msg import UInt8, UInt16MultiArray
@@ -16,7 +18,7 @@ import matplotlib.pyplot as plt # Used for plotting and error checking
 
 # Parameters
 MAX_STEERING_ANGLE = 0.442  # [rad]
-CONSTANT_THRUST = float(0.5)  # [0 to 2.5]
+CONSTANT_THRUST = float(0.2)  # [0 to 2.5]
 KP = 0.05  # Proportional gain constant
 
 class LineFollower(Node):
@@ -25,7 +27,7 @@ class LineFollower(Node):
 
         # Logic variables
         self.emergency_stop = False
-        self.shutdowned = False
+        self.destroyed = False
 
         # Define messages
         self.ack_msg = AckermannDrive()
@@ -37,8 +39,27 @@ class LineFollower(Node):
         # Initialize publisher
         self.ackermann_pub = self.create_publisher(AckermannDrive, '/ackermann_cmd', 10)
 
+        # Register shutdown callback (function triggered at ctr+c) 
+        signal.signal(signal.SIGINT, self.shutdown_callback)
+
         # Initialize CvBridge
         self.bridge = CvBridge()
+
+    def shutdown_callback(self, signum, frame):
+          if self.destroyed:
+            return
+
+          self.get_logger().info('Node terminated!')
+
+          # Send ackermann_halt for 1 second
+          t0 = self.get_clock().now().to_msg().sec
+          t_close = 1
+          while (self.get_clock().now().to_msg().sec - t0) < t_close:
+            self.send_ackermann_halt()
+
+          self.node.destroy_node()    ## TODO: fix this
+          self.destroyed = True
+          rclpy.shutdown()
 
     def emergency_shutdown_callback(self, joy_msg):
         if joy_msg.buttons == 2:
@@ -114,9 +135,9 @@ class LineFollower(Node):
 
     def send_ackermann(self, steering_angle):
         ack_msg = AckermannDrive()
-        ack_msg.steering_angle = 0.0
+        ack_msg.steering_angle = steering_angle
         ack_msg.steering_angle_velocity = 0.0
-        ack_msg.speed = 0.1
+        ack_msg.speed = CONSTANT_THRUST
         ack_msg.acceleration = 0.0
         ack_msg.jerk = 0.0
         self.ackermann_pub.publish(ack_msg)
@@ -126,15 +147,6 @@ class LineFollower(Node):
         ack_msg.steering_angle = 0.0
         ack_msg.speed = 0.0
         self.ackermann_pub.publish(ack_msg)
-
-    def hook(self):
-        self.get_logger().info('Initiate shutdown!')
-        self.shutdowned = True
-        t0 = self.get_clock().now().to_msg().sec
-        t_close = 1
-
-        while (self.get_clock().now().to_msg().sec - t0) < t_close:
-            self.send_ackermann(0)
             
     def crop_image(self, image):
         if image is None:
@@ -794,9 +806,8 @@ def crop_image(image):
 def main(args=None):
     rclpy.init(args=args)
     node = LineFollower()
+
     rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
